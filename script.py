07b240674ffddc82a46209ab56338fffd34abb83
ahluwa41 <saksham.ahluwalia@mail.utoronto.ca>
2018-11-06 18:59:29 -0500

    removed the for loop

--- script.py
+++ script.py
@@ -1,23 +1,34 @@
 import random, requests, shutil, os, time
 from bs4 import BeautifulSoup
 
+start_time = time.time()
+
 page_number = random.randint(1, 9)
 
+if page_number == 1:
+	index = random.randint(1, 30)
+elif page_number == 9:
+	index = random.randint(0, 16)
+else:
+	index = random.randint(0, 30)
+
 # Request the page
-req = requests.get("https://workhardanywhere.com/wallpapers/page/{}/".format(page_number))
+if page_number == 1:
+	req = requests.get("https://workhardanywhere.com/wallpapers")
+else:
+	req = requests.get("https://workhardanywhere.com/wallpapers/{}/".format(page_number))
+
 content = req.content
 
 # Create an instance of BeautifulSoup
 soup = BeautifulSoup(content, "html.parser")
 
-# Get the image on the page. approx 30
+# Get the images on the page. approx 30
 images = soup.findAll("div", {"class": "image_holder"})
 
-# capture their urls
-sources = [img.find("img")['src'].replace("-550x550", "") for img in images]
-index = random.randint(2, 30)
+# capture a randomn image's url
+wallpaper = images[index].find("img")['src'].replace("-550x550", "")
 
-wallpaper = sources[index]
 data = requests.get(wallpaper, stream=True)
 with open('//home/ahlus/Wallpapers/wallpaper.jpg', 'wb') as out_file:
     shutil.copyfileobj(data.raw, out_file)
@@ -25,6 +36,4 @@ with open('//home/ahlus/Wallpapers/wallpaper.jpg', 'wb') as out_file:
 del data
 
 os.system("gsettings set org.gnome.desktop.background picture-uri file:///home/ahlus/Wallpapers/wallpaper.jpg")
-time.sleep(1)
-# os.system("rm wallpaper.jpg")
-print ("enjoy")
+print("--- %s seconds ---" % (time.time() - start_time))
