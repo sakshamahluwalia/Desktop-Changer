b35c53710299e198bdce429e9455370260d95009
Saksham Ahluwalia <sakshamahluwalia5@gmail.com>
2020-01-04 17:44:42 -0500

    Had to change the logic since the website layout got changed

--- script.py
+++ script.py
@@ -3,31 +3,41 @@ from bs4 import BeautifulSoup
 
 start_time = time.time()
 
-page_number = random.randint(1, 9)
+page_number = random.randint(1, 2) 
 
 if page_number == 1:
-	index = random.randint(1, 30)
-elif page_number == 9:
-	index = random.randint(0, 16)
+	index = random.randint(1, 48)
 else:
-	index = random.randint(0, 30)
+	index = random.randint(0, 26)
 
 # Request the page
 if page_number == 1:
-	req = requests.get("https://workhardanywhere.com/wallpapers")
+	req = requests.get("https://workhardanywhere.com/collections/wallpapers")
 else:
-	req = requests.get("https://workhardanywhere.com/wallpapers/{}/".format(page_number))
+	req = requests.get("https://workhardanywhere.com/wallpapers?page={}".format(page_number))
 
 content = req.content
 
 # Create an instance of BeautifulSoup
 soup = BeautifulSoup(content, "html.parser")
 
-# Get the images on the page. approx 30
-images = soup.findAll("div", {"class": "image_holder"})
+# Get the link to the image page 
+tags = soup.findAll("a", {"class": "ProductItem__ImageWrapper"})
+
+# extract the link from the anchor tag 
+link = "https://shop.workhardanywhere.com/" + tags[index]["href"]
+
+req2 = requests.get(link)
+
+content_req2 = req2.content
+
+# Create an instance of BeautifulSoup
+soup_req2 = BeautifulSoup(content_req2, "html.parser")
+
+images = soup_req2.findAll("a", {"class": "wallpaper_download"})
 
 # capture a randomn image's url
-wallpaper = images[index].find("img")['src'].replace("-550x550", "")
+wallpaper = images[0]["href"]
 
 data = requests.get(wallpaper, stream=True)
 with open('//home/ahlus/Wallpapers/wallpaper.jpg', 'wb') as out_file:
